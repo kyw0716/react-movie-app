a3c4790889cba20238a60377cc4f6fd02d01e8e4
kyw0716 <kyw0716@naver.com>
2022-01-10 00:38:40 +0900

    Use Router
    Movie페이지 추가

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -3,11 +3,15 @@ import{
   Switch,
   Route,
 } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return <Router>
     <Switch>
+      <Route path="/movie">
+        <Detail />
+      </Route>
       <Route path="/">
         <Home />
       </Route>
--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({ id, coverImg, title, summary, movie, genres }) {
     return (
         <div key={id}>
             <img src={coverImg} alt="cover_img" />
-            <h2>{title}</h2>
+            <h2>
+                <Link to="/movie">{title}</Link>
+            </h2>
             <p>{summary}</p>
             <ul>
                 {(movie.hasOwnProperty("genres")) ? genres.map((g) => (
